5a894c04327bc4d3d9d0ad277f53011bb1753a5e
StevenEscobar14 <stevenescobaraw@gmail.com>
2023-11-28 18:10:08 -0500

    Add: gitignore

---
+++ main.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+import keras
+from keras.models import Sequential
+from keras.layers import InputLayer, Input, MaxPool2D, Flatten, Conv2D, Dense, Reshape
+
