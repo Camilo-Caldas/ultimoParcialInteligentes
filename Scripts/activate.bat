4297cadfa5354b6721d4477a53006e90fa422bfd
StevenEscobar14 <stevenescobaraw@gmail.com>
2023-11-28 17:48:05 -0500

    Add: Environment local

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\killz\OneDrive\Documentos\ultimoParcialInteligentes\PARCIAL2
+set VIRTUAL_ENV=D:\Estudio\Inteligentes 2\ultimoParcialInteligentes
 
 if not defined PROMPT set PROMPT=$P$G
 
