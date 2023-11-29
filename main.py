5cdb3d6c38fdeca86f5bc258ff208764f335841d
StevenEscobar14 <stevenescobaraw@gmail.com>
2023-11-28 20:48:35 -0500

    Add: pruebas y cambios gitignore

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import datetime
 import os
 import random
+from typing import Concatenate
 import tensorflow as tf
 import numpy as np
 import cv2
 import keras
-from keras.models import Sequential
-from keras.layers import InputLayer, Input, MaxPool2D, Flatten, Conv2D, Dense, Reshape
+from keras.models import Sequential, Model
+from keras.layers import InputLayer, Input, MaxPool2D, Flatten, Conv2D, Dense, Reshape, Dropout, Activation, Concatenate
 from keras.applications import VGG16, ResNet50
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input as vgg_preprocess_input
 from keras.applications.resnet50 import preprocess_input as resnet_preprocess_input
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+import seaborn as sns
+from keras.callbacks import ModelCheckpoint
 
 # def cargarDatos(rutaOrigen, numeroCategorias, limite, ancho, alto):
 #     imagenesCargadas = []
@@ -81,11 +87,6 @@ print(label_dict)
 print(categories)
 print(labels)
 
-ruta_absoluta = "D:/Estudio/Inteligentes 2/ultimoParcialInteligentes/dataset/train/COVID"
-directorio_base = "D:/Estudio/Inteligentes 2/ultimoParcialInteligentes/"
-
-ruta_relativa = os.path.relpath(ruta_absoluta, directorio_base)
-print(ruta_relativa)
 
 #"COVID-19"
 folder_path_covid =[
@@ -121,7 +122,7 @@ covid_images_on_data_set = len(img_path_covid_old)
 covid_non_images_on_data_set = len(img_path_non_covid_old)
 Total_images_on_data_set = covid_images_on_data_set + covid_non_images_on_data_set
 
-print(f" Total Images on the data set : {Total_images_on_data_set}\n Covid Images : {covid_images_on_data_set}\n Non Covid Images : {Total_images_on_data_set}\n")    
+print(f" Total Images on the data set : {Total_images_on_data_set}\n Covid Images : {covid_images_on_data_set}\n Non Covid Images : {covid_non_images_on_data_set}\n")    
         
 # images array shuffles
 img_path_covid_old = random.sample(img_path_covid_old, len(img_path_covid_old))
@@ -143,6 +144,155 @@ print(f"Non covid images count : {len(img_path_non_covid)}")
 print(f"All images count       : {total_images}") 
 print(f"Covid images           : {round((len(img_path_covid)/total_images)*100, 2)} %")           
 print(f"Non covid images       : {round((len(img_path_non_covid)/total_images)*100, 2)} %")  
+
+start_time = datetime.datetime.now()
+print(f"Start images reading :{start_time}")
+img_size=256
+data=[]
+target=[]
+
+def image_add(img_path,category) :
+    img=cv2.imread(img_path)
+    try:
+        gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)           
+                    #Coverting the image into gray scale
+        resized=cv2.resize(gray,(img_size,img_size))
+                    #resizing the gray scale into 256x256, since we need a fixed common size for all the images in the dataset
+        data.append(resized)
+        target.append(label_dict[category])
+                    #appending the image and the label(categorized) into the list (dataset)
+
+    except Exception as e:
+        print('Exception:',e)
+                    #if any exception rasied, the exception will be printed here. And pass to the next image   
+
+for category in categories:
+    if(category == "COVID-19"):
+        for img_path in img_path_covid:
+            image_add(img_path,category)  
+    else:
+        for img_path in img_path_non_covid:
+            image_add(img_path,category)  
+
+                    
+end_time = datetime.datetime.now()                 
+print(f"End images reading :{end_time}")
+
+print(f"Total time taken {end_time-start_time}")
+
+data=np.array(data)/255.0
+data=np.reshape(data,(data.shape[0],img_size,img_size,1))
+target=np.array(target)
+
+
+new_target=keras.utils.to_categorical(target)
+
+
+print(f"Data shape : {data.shape} \nTarget shape : {target.shape} \nNew Target shape : {new_target.shape}")
+
+input_shape=data.shape[1:] #50,50,1
+inp=Input(shape=input_shape)
+convs=[]
+
+parrallel_kernels=[3,5,7,9]
+
+for k in range(len(parrallel_kernels)):
+    if(k !=0):
+        conv = Conv2D(128, kernel_size = k,padding = 'same' ,activation='relu')(inp)
+
+        convs.append(conv)
+
+out = Concatenate()(convs)
+conv_model = Model(inp, out)
+
+model = Sequential()
+model.add(conv_model)
+
+# Hidden Layer 1
+model.add(Conv2D(128,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+
+# Hidden Layer 2
+model.add(Conv2D(64,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+
+# Hidden Layer 3
+model.add(Conv2D(32,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+
+# Output layer
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(4,input_dim=128,activation='sigmoid'))
+
+model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+model.summary()
+
+train_data,test_data,train_target,test_target=train_test_split(data,target,test_size=0.3)
+
+print(type (train_data))
+print(f"Training Data shape : {train_data.shape} \nTraining Target shape : {train_target.shape}")
+print(f"Test Data shape : {test_data.shape} \nTest Target shape : {test_target.shape}")
+
+# start_time = datetime.datetime.now()
+# print(f"Start checkpoint creation:{start_time}")
+
+# checkpoint = ModelCheckpoint('model-{epoch:03d}.model',
+#                              monitor='val_accuracy',
+#                              verbose=1,
+#                              save_best_only=True,
+#                              mode='max')
+
+
+# history=model.fit(train_data,
+#                   train_target,
+#                   epochs=25,
+#                   validation_split=0.1)
+#end_time = datetime.datetime.now()
+
+
+# print(f"End checkpoint creation:{end_time}")
+# print(f"Total time taken {end_time-start_time}")
+
+# floting values in to graph
+
+# sns.set()
+# acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+
+# epochs = range(0,len(acc))
+# fig = plt.gcf()
+# fig.set_size_inches(16, 8)
+
+# plt.plot(epochs, acc, 'r', label='Training accuracy',marker = "o")
+# plt.plot(epochs, val_acc, 'b', label='Validation accuracy',marker = "o")
+# plt.title('Training and validation accuracy Model 1')
+# plt.xticks(np.arange(0, len(acc), 10))
+# plt.legend(loc=0)
+# plt.figure()
+
+# fig = plt.gcf()
+# fig.set_size_inches(16, 8)
+# plt.plot(epochs, loss, 'r', label='Training Loss',marker = "o")
+# plt.plot(epochs, val_loss, 'b', label='Validation Loss',marker = "o")
+# plt.title('Training and validation Loss Model 1')
+# plt.xticks(np.arange(0, len(acc), 10))
+# plt.legend(loc=0)
+# #plt.savefig('Multiclass Model .png')
+# plt.figure()
+# plt.show()
+
+print(model.evaluate(test_data,test_target))
 
 # cantidaDatosEntrenamiento = [60, 60, 60, 60, 60, 60, 60, 60, 60, 60]
 # cantidaDatosPruebas = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20]
